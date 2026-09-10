7dad885046a671a763a7709431a374033f6362e7
Felix Zotz <felix.zotz@gmail.com>
2026-09-10 19:07:53 +0200

    refactor(firefox): remove unnecesary extensions

--- modules/home/firefox.nix
+++ modules/home/firefox.nix
@@ -13,21 +13,9 @@
           installation_mode = "force_installed";
         };
 
-        # Bitwarden
-        "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
-          install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
-          installation_mode = "force_installed";
-        };
-
         # SponsorBlock
         "sponsorBlocker@ajay.app" = {
           install_url = "https://addons.mozilla.org/firefox/downloads/latest/sponsorblock/latest.xpi";
-          installation_mode = "force_installed";
-        };
-
-        # Sidebery
-        "{3c078156-979c-498b-8990-85f7987dd929}" = {
-          install_url = "https://addons.mozilla.org/firefox/downloads/latest/sidebery/latest.xpi";
           installation_mode = "force_installed";
         };
 
